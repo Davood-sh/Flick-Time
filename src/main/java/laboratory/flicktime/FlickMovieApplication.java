49805ba6da4b9074d07ebd484205add2c320cea2
Davood-sh <116182634+Davood-sh@users.noreply.github.com>
2024-08-03 19:36:01 +0200

    Last Version

---
+++ src/main/java/laboratory/flicktime/FlickMovieApplication.java
@@ -0,0 +1,13 @@
+package laboratory.flicktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickMovieApplication.class, args);
+	}
+
+}
